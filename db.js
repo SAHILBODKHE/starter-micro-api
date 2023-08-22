fc0aeae26756e810b6822a2f3303f8f8e3eb26f0
Sahil Sambhaji Bodkhe <79392045+SAHILBODKHE@users.noreply.github.com>
2023-08-22 11:18:44 +0530

    Update db.js

--- db.js
+++ db.js
@@ -9,9 +9,9 @@ const { Pool } = require('pg')
 // })
 const pool = new Pool({
   database: 'railway',
-  host: 'containers-us-west-186.railway.app',
-  password: 'ufdrMiV4osmJkS5Md3Yc',
-  port: 6928,
+  host: 'containers-us-west-133.railway.app',
+  password: 'LAXIVzON8VXNL9ChDeH2',
+  port: 7694,
   user: 'postgres',
 })
 module.exports = pool
